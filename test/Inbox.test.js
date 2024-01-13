4be8defeabf49b86983e6391c8f4ac20f8976d69
Juan Brundi Sarmento Madeira <juanbrundism@gmail.com>
2024-01-13 16:27:57 +0800

    feat: accessing contract method

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,14 +6,15 @@ const { abi, evm } = require("../compile")
 
 let accounts;
 let inbox;
+const INITIAL_MESSGE = "Halo"
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
-  
+
   inbox = await new web3.eth.Contract(abi)
     .deploy({
       data: evm.bytecode.object,
-      arguments: ["Halo"]
+      arguments: [INITIAL_MESSGE]
     })
     .send({
       from: accounts[0],
@@ -23,6 +24,24 @@ beforeEach(async () => {
 
 describe("Inbox", () => {
   it("deploy contract", () => {
-    console.log(inbox)
+    assert.ok(inbox.options.address)
   })
+
+  it("has initial message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSGE)
+  })
+
+  it("can change message value", async () => {
+    await inbox.methods
+      .setMessage("Hai")
+      .send({
+        from: accounts[0],
+        gas: "1000000"
+      })
+
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "Hai")
+  })
+
 })
