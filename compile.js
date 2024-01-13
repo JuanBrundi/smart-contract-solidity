65ba1b34b22600b44ab52a18a9b54ff75d0abf87
Juan Brundi Sarmento Madeira <juanbrundism@gmail.com>
2024-01-13 15:36:56 +0800

    feat: update solidity version and syntax

--- compile.js
+++ compile.js
@@ -2,7 +2,27 @@ const path = require("path")
 const fs = require("fs")
 const solc = require("solc")
 
-const inboxPath =   path.resolve(__dirname, "contracts", "Inbox.sol")
+const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol")
 const source = fs.readFileSync(inboxPath, "utf-8")
 
-console.log(solc.compile(source, 1));
+const input = {
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source
+    }
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"]
+      }
+    }
+  }
+}
+
+const compiledData = JSON
+  .parse(solc.compile(JSON.stringify(input)))
+  .contracts["Inbox.sol"]["Inbox"]
+
+module.exports = compiledData
